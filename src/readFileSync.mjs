77ac4bf0e8eabdf7370f243222849d48a9e2a9fd
Yurii Tykhonov <yuryi.tykhonoff@machinemax.com>
2026-02-22 00:13:12 +0200

    Add file for reading the file synchronisly

---
+++ src/readFileSync.mjs
@@ -0,0 +1,33 @@
+import fs from "node:fs"
+import { fileURLToPath } from "node:url"
+import { dirname, join } from "node:path"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+const filePath = join(__dirname, "..", "data_source.txt")
+// we can try to check whether the file exists using fs.access(filePath)
+
+const OPERATION_SYSTEM_ERROR_CODE_FOR_NON_EXISTENT_FILE_OR_DIRECTORY = "ENOENT"
+console.log("Current directory:", __dirname)
+console.log("Starting to read file...")
+
+try {
+  const data = fs.readFileSync(filePath, "utf8")
+  // it is the same as  fs.readFileSync("file.txt").toString("utf8")
+  // cause by default, it returns a buffer,
+  // so we need to convert it to string using toString() method or by passing "utf8" as the second argument.
+
+  console.log("File reading completed.")
+  const lines = data.split("\n").filter((line) => line.trim() !== "")
+  console.log("Number of lines:", lines.length)
+  console.log("First line:", lines[0])
+} catch (err) {
+  if (
+    err.code === OPERATION_SYSTEM_ERROR_CODE_FOR_NON_EXISTENT_FILE_OR_DIRECTORY
+  ) {
+    console.error("File not found:", filePath)
+  } else {
+    console.error("Error reading file:", err)
+    throw err
+  }
+}
