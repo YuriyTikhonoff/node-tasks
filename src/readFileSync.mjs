17c22ac351173bd2996164ed0c0e4a08e340b732
Yurii Tykhonov <yuryi.tykhonoff@machinemax.com>
2026-02-22 00:40:37 +0200

    Create read sync file

--- src/readFileSync.mjs
+++ src/readFileSync.mjs
@@ -5,15 +5,18 @@ import { dirname, join } from "node:path"
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 const filePath = join(__dirname, "..", "data_source.txt")
-// we can try to check whether the file exists using fs.access(filePath)
+// we can try to check whether the file exists using
+// try { await fs.access(filePath) } catch (err) { console.error("File not found") }
 
 const OPERATION_SYSTEM_ERROR_CODE_FOR_NON_EXISTENT_FILE_OR_DIRECTORY = "ENOENT"
+const ENCODING = "utf8"
+
 console.log("Current directory:", __dirname)
 console.log("Starting to read file...")
 
 try {
-  const data = fs.readFileSync(filePath, "utf8")
-  // it is the same as  fs.readFileSync("file.txt").toString("utf8")
+  const data = fs.readFileSync(filePath, ENCODING)
+  // it is the same as  fs.readFileSync("file.txt").toString(ENCODING)
   // cause by default, it returns a buffer,
   // so we need to convert it to string using toString() method or by passing "utf8" as the second argument.
 
